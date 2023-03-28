24b29e652c53e8385fd5585c52f3ba16f38a450f
Wisho <zap707@amerike.edu.mx>
2023-03-28 10:53:32 -0600

    Trabajando en la flecha

--- Assets/Scrips/Arrow.cs
+++ Assets/Scrips/Arrow.cs
@@ -1,38 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Arrow : MonoBehaviour
 {
-    public float x;
-    public bool lateStart;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (lateStart == false)
-        {
-            x = transform.localEulerAngles.z;
-            lateStart = true;
-        }
-        transform.rotation = Quaternion.Euler(0, 0, x);
-    }
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.tag == "Circle")
-        {
-            x -= 5;
-        }
-        if (other.gameObject.tag == "Ground")
-        { 
-            this.transform.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
-        }
-    }
-
+ 
 }
